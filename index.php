9172979e8f262f00df32a8f6a64230bdd09a823b
Daniel Billotte <daniel@litfuze.com>
2014-05-19 13:51:18 -0700

    fixed some bugs

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	//exit;
 ?>
 <?php
-	//if($_POST) { ... } 
 	$error_array = array();
+	$error_string = "";
 	if($_SERVER['REQUEST_METHOD'] == 'POST') {
 		if($_POST['first_name'] == '') {
 			array_push($error_array, 'Please enter a first name');
@@ -15,13 +15,15 @@
 		if($_POST['last_name'] == '') {
 			array_push($error_array, 'Please enter a last name');
 		}
-	}
-	$error = "";
-	if(count($error_array) > 0) {
-		foreach($error_array as $e) {
-			$error = $error . $e . "<br>"; 
+
+		$error = "";
+		if(count($error_array) > 0) {
+			foreach($error_array as $e) {
+				$error_string = $error_string . $e . "<br>"; 
+			}
 		}
 	}
+
 
 ?>
 
@@ -32,10 +34,10 @@
 	<title>Hello PHP Forms</title>
 </head>
 <body>
-	<div class="error"><?php print_r($error); ?></div>
+	<div class="error"><?php print_r($error_string); ?></div>
 	<form action="" method="POST">
-		FName: <input type="text" name="first_name" value="<?php echo($_POST['first_name']);?>"><br>
-		LName: <input type="text" name="last_name" value="<?php echo($_POST['last_name']);?>"><br>
+		FName: <input type="text" name="first_name" value=""><br>
+		LName: <input type="text" name="last_name" value=""><br>
 		Email: <input type="email" name="email"><br>
 		<button type="submit">Let's Go!</button>
 	</form>
